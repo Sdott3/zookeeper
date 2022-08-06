9dfd18a9657a94e4e2735b50dceaf7ad3ae48675
sdott3 <sdotthomas347@gmail.com>
2022-08-05 22:42:47 -0400

    add Heroku

--- server.js
+++ server.js
@@ -12,55 +12,16 @@ const path = require('path');
 // route to express.js
 app.use(express.static('public'));
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
 // parse incoming JSON data
 app.use(express.json());
-  
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-  
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
-});
 
-app.post('/api/animals', (req, res) => {
-    // set id based on what the next index of the array will be 
-    req.body.id = animals.length.toString();
-
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validateAnimal(req.body)) {
-      resizeTo,staus(400).send('The animal is not properly formatted. ');
-    } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal); 
-    }
-});
-
-// create routes to serve index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// create route to take user to animals
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-// create route to take user to zookeeper
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // starts port
 app.listen(PORT, () => {
